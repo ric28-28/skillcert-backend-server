c544522e2a6964de7ced38672bce042ca60b4f58
Deedee <deedeeowoeye@gmail.com>
2025-05-30 18:20:03 +0100

    chore: setup mssql database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { databaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
