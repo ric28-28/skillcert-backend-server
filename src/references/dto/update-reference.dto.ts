0fddfd63ab8d01e729c54e245d295d1303b2138c
laryhills <hilaryokoh@gmail.com>
2025-08-18 20:09:58 +0100

    fix: add space in UpdateReferenceDto class definition for consistency

--- src/references/dto/update-reference.dto.ts
+++ src/references/dto/update-reference.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateReferenceDto } from './create-reference.dto';
 
-export class UpdateReferenceDto extends PartialType(CreateReferenceDto) {}
+export class UpdateReferenceDto extends PartialType(CreateReferenceDto) { }
