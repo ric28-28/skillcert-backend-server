b5ed054a15fbc06fedc9e61b94eef8a184a327d2
icedoutskay <somkeneodikpo@gmail.com>
2025-08-26 12:59:19 +0100

    implemnt course progress tracking

---
+++ src/course-progress/entities/course-progress.entity.ts
@@ -0,0 +1,46 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  UpdateDateColumn,
+  CreateDateColumn,
+  Unique,
+} from 'typeorm';
+import { Enrollment } from '../../enrollment/entities/enrollment.entity';
+import { Lesson } from '../../lessons/entities/lesson.entity';
+
+export enum ProgressStatus {
+  NOT_STARTED = 'not_started',
+  IN_PROGRESS = 'in_progress',
+  COMPLETED = 'completed',
+}
+
+@Entity('course_progress')
+@Unique(['enrollment', 'lesson']) // prevent duplicates
+export class CourseProgress {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Enrollment, (enrollment) => enrollment.progress, {
+    onDelete: 'CASCADE',
+  })
+  enrollment: Enrollment;
+
+  @ManyToOne(() => Lesson, { eager: true, onDelete: 'CASCADE' })
+  lesson: Lesson;
+
+  @Column({
+    type: 'enum',
+    enum: ProgressStatus,
+    default: ProgressStatus.NOT_STARTED,
+  })
+  status: ProgressStatus;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
