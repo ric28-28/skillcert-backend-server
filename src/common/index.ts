e1239f8b87561594977cbf49af90ef71f1a16521
KAMALDEEN333 <kamalaliyu503@gmail.com>
2025-09-11 19:43:53 +0100

    Updated index

--- src/common/index.ts
+++ src/common/index.ts
@@ -8,3 +8,4 @@ export * from './decorators/roles.decorator';
 // Filters
 export * from './filters/all-exceptions.filter';
 export * from './filters/forbidden-exception.filter';
+export * from './filters/database-exception.filter';
