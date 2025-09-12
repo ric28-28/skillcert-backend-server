eb0a204b688b5693a28e45d667d43f778345650a
tecch-wiz <zynstro@gmail.com>
2025-09-12 23:01:57 +0100

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Roles } from '../common/decorators/roles.decorator';
+import { AuthGuard } from '../common/guards/auth.guard';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { UserRole } from '../users/entities/user.entity';
 import { AppService } from './app.service';
 
 @Controller()
+@UseGuards(AuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
